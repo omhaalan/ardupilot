bd5da09b0abf2d2a79b5bb11b4554883704d471c
Ole Haaland <omhaalan@stud.ntnu.no>
2021-02-07 15:06:19 +0100

    added speed step

--- ArduPlane/scripts/airspeedStep.lua
+++ ArduPlane/scripts/airspeedStep.lua
@@ -1,2 +1,24 @@
 local baseline = param:get("TRIM_ARSPD_CM")
+local flag = 0
+ 
+
+function airspeedStep()
+	current = mission:get_current_nav_index()
+
+	if current > 4 then
+		if flag == 0
+			prev = current
+			flag = 1
+		end
+		if current ~= prev then
+			prev = current
+			gain = flag * 0.1 * baseline
+			flag = flag + 1
+			param:set("TRIM_ARSPD_CM", baseline+gain)
+		end
+	end
+	return airspeedStep, 100
+end
+
+return airspeedStep(), 100
 
